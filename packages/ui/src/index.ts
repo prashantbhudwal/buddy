22c907d327d4163d13c6213c667821095b148d3d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-02-23 16:56:55 +0530

    chore: add all shadcn components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,36 @@
 export { Button, buttonVariants } from "./components/ui/button";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
 export { Input } from "./components/ui/input";
+export { NativeSelect, NativeSelectOptGroup, NativeSelectOption } from "./components/ui/native-select";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+export { Textarea } from "./components/ui/textarea";
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./components/ui/tooltip";
 export { cn } from "./lib/utils";
