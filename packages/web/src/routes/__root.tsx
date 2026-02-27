7114608e4ff40a155f4570338aea12f947b77b68
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-02-27 12:14:03 +0530

    chore(web): Position TanStack Router devtools at bottom-right

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen bg-background text-foreground">
       <Outlet />
-      <TanStackRouterDevtools />
+      <TanStackRouterDevtools position="bottom-right" />
     </div>
   ),
   notFoundComponent: () => <div className="p-6">404 Not Found</div>,
